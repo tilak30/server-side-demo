06186438c6ee82faa9622ea62953e9f7bdcd3879
tilak30 <tilakbhansali3009@gmail.com>
2020-09-27 18:14:25 +0530

    Express sessions part-2

--- app.js
+++ app.js
@@ -42,40 +42,25 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 function auth (req, res, next) {
   console.log(req.session);
 
   if (!req.session.user) {
-      var authHeader = req.headers.authorization;
-      if (!authHeader) {
-          var err = new Error('You are not authenticated!');
-          res.setHeader('WWW-Authenticate', 'Basic');                        
-          err.status = 401;
-          next(err);
-          return;
-      }
-      var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-      var user = auth[0];
-      var pass = auth[1];
-      if (user == 'admin' && pass == 'password') {
-          req.session.user = 'admin';
-          next(); // authorized
-      } else {
-          var err = new Error('You are not authenticated!');
-          res.setHeader('WWW-Authenticate', 'Basic');
-          err.status = 401;
-          next(err);
-      }
+      var err = new Error('You are not authenticated!');                      
+      err.status = 403;
+      return next(err);
   }
   else {
-      if (req.session.user === 'admin') {
-          console.log('req.session: ',req.session);
+      if (req.session.user === 'authenticated') {
           next();
       }
       else {
           var err = new Error('You are not authenticated!');
-          err.status = 401;
-          next(err);
+          err.status = 403;
+          return next(err);
       }
   }
 }
@@ -84,8 +69,6 @@ app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
