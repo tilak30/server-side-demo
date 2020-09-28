143c92db6ea77a185fc3c67dcb76f2518c58fc19
tilak30 <tilakbhansali3009@gmail.com>
2020-09-28 18:41:58 +0530

    Passport JWT

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 const bodyParser = require('body-parser');
 var User = require('../models/user');
 var passport = require('passport');
+var authenticate = require('../authenticate');
 
 var router = express.Router();
 router.use(bodyParser.json());
@@ -31,10 +32,13 @@ router.post('/signup', (req, res, next) => {
 });
 
 //using passport mongoose
-router.post('/login', passport.authenticate('local'), (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.json({success: true, status: 'You are successfully logged in!'});
+router.post('/login', passport.authenticate('local'), (req,
+   res) => {
+
+    var token = authenticate.getToken({_id: req.user._id});
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({success: true, token: token, status: 'You are successfully logged in!'});
 });
 
 
